42319d8d92538c91f53e41582168327f31fc0bf0
Parth <parth.dhami@myglamm.com>
2023-08-24 18:51:00 +0530

    Harmoney assignment

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -46,21 +46,23 @@ const Messages = ({ messages, handleDeleteMessage }) => {
         </div>
       ))}
 
-      <div className="paginationContainer">
-        <button
-          className="Cta"
-          onClick={() => {
-            if (pageNumber === 1) return;
-            setPageNumber(pageNumber - 1);
-          }}
-        >
-          Prev
-        </button>
-        <div className="pageNumber">{pageNumber}</div>
-        <button className="Cta" onClick={() => setPageNumber(pageNumber + 1)}>
-          Next
-        </button>
-      </div>
+      {messages.length > 5 && (
+        <div className="paginationContainer">
+          <button
+            className="Cta"
+            onClick={() => {
+              if (pageNumber === 1) return;
+              setPageNumber(pageNumber - 1);
+            }}
+          >
+            Prev
+          </button>
+          <div className="pageNumber">{pageNumber}</div>
+          <button className="Cta" onClick={() => setPageNumber(pageNumber + 1)}>
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 };
